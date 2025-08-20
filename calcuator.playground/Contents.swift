e6ad7db93b036f3da58cfb2b8239873edee537e7
104hyeon <104hyeon@gmail.com>
2025-08-20 17:02:46 +0900

    Initial Commit

---
+++ calcuator.playground/Contents.swift
@@ -0,0 +1,37 @@
+//import UIKit
+
+class Calculator {
+    
+    func sum(firstNumber: Int, secondNumber: Int) -> Int {
+        return firstNumber + secondNumber
+    }
+    func subtract(firstNumber: Int, secondNumber: Int) -> Int {
+        return firstNumber - secondNumber
+    }
+    
+    func multiply(firstNumber: Int, secondNumber: Int) -> Int {
+        return firstNumber * secondNumber
+    }
+    
+    func divide(firstNumber: Int, secondNumber: Int) -> Double {
+        guard secondNumber != 0 else {
+            return 0
+        }
+        return Double(firstNumber) / Double(secondNumber)
+    }
+    
+}
+
+let calcuator = Calculator()
+
+let sumResult = calcuator.sum(firstNumber: 10, secondNumber: 15)
+print(sumResult)
+
+let subtractResult = calcuator.subtract(firstNumber: 20, secondNumber: 7)
+print(subtractResult)
+
+let multiplyResult = calcuator.multiply(firstNumber: 5, secondNumber: 9)
+print(multiplyResult)
+
+let divideResult = calcuator.divide(firstNumber: 34, secondNumber: 0)
+print(divideResult)
